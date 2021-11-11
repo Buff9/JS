af9e9cc542aa258eae7198343c2b780f8cbee4b0
Buff9 <yura.korovickiy9583@gmail.com>
2021-11-11 23:12:43 +0300

    1

---
+++ lesson1/errors/script.js
@@ -0,0 +1,20 @@
+let a = 100;
+let b = 500;
+let c = "hello";
+let d = "okten";
+let x = 'constant value';
+
+{
+    console.log(x);
+    x = 'new value';
+}
+
+let result = a + b;
+console.log(result);
+
+b = 'new value';
+
+document.write(c," ", d);
+
+
+
