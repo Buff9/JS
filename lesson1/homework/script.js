04f5cf375579d3107cb11faeed0ccf4373dbbdb7
Buff9 <yura.korovickiy9583@gmail.com>
2021-11-11 17:43:10 +0300

    Initial commit

---
+++ lesson1/homework/script.js
@@ -0,0 +1,99 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let a = 'hello ';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// alert(a + b+"." + c+"." + d);
+//
+// let e = '1';
+// let f = '10';
+// let g = '-999';
+// let h = '123';
+// let i = '16';
+// let j = '2.7';
+// let k = '3.14';
+// document.write(e, f, g, h, i, j, k);
+//
+// console.log(9 > 8);
+// console.log(!!0);
+
+
+
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let a = 'hello ';
+// let b = 'world';
+// alert(a + b);
+//
+// let e = '1';
+// let f = '10';
+// let g = '11';
+// let h = '01';
+// let i = '101';
+// let j = '111';
+// let k = '110';
+// document.write(e, f, g, h, i, j, k);
+//
+// console.log(9 > 8);
+// console.log(!!0);
+
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person.
+
+// let firstName = 'Yurii ';
+// let middleName = 'Romanovich ';
+// let lastName = 'Korovickiy';
+// let person = firstName + middleName + lastName;
+// console.log(person);
+
+
+
+//- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// let name = prompt('імя');
+// let middlename = prompt('По-Батькові');
+// let age = prompt('Вік');
+// alert('Вітаю ' + name + middlename + '. Тобі' + age + ' років');
+
+
+
+//- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+
+//- Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(5 === 6);
+// console.log(10 == 10);
+// console.log(10 === 10);
+// console.log(10 > 10);
+// console.log(10 < 10);
+// console.log(10 != 10);
+// console.log(123 === '123');
+// console.log(123 !== '123');
+
+//В математических функциях и выражениях происходит численное преобразование.
+
+let str = "20";
+let a = 5;
+
+document.write(str + a + "<br/>"); //str(20) + a(5) = 25
+document.write(str - a + "<br/>"); //str(20) - a(5) = 15
+document.write(str * "2" + "<br/>"); //str(20) * "2" = 40
+document.write(str / 2 + "<br/>"); //str(20) / "2" = 10
