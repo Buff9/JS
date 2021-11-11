c612d9dc9d6cd1e9d6951c72bd137b327fafc682
Buff9 <yura.korovickiy9583@gmail.com>
2021-11-11 22:46:48 +0300

    1

--- lesson1/classwork/script.js
+++ lesson1/classwork/script.js
@@ -1,29 +1,74 @@
-let products = [
-    {
-        title: 'milk',
-        price: 22,
-        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
-    },
-    {
-        title: 'juice',
-        price: 27,
-        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-    },
-    {
-        title: 'tomato',
-        price: 47,
-        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-    },
-    {
-        title: 'tea',
-        price: 15,
-        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-    },
-];
-for (const product of products) {
-    document.write(
-        `<div>
-                        <h2>${product.title} ${product.price}</h2>
-                        <img src="${product.image}" alt="">
-                </div>`)
-}
+//1.
+// let arr = [123, 34, 23, 65, 12, 45, 87, 44, 13, 16];
+// let result = (arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + arr[7] + arr[8] + arr[9]);
+// console.log(result);
+
+
+
+//2.
+// let book = new Object();
+// book.name = "Odyssey";
+// book.page = "438";
+// book.genre = "epic poem";
+// console.log (book);
+
+
+
+//3.
+// let book = new Object();
+// book.name = "Odyssey";
+// book.page = "438";
+// book.genre = "epic poem";
+// book.author = "Homer";ory genre
+// console.log (book);
+
+
+
+//4.
+// let book = [
+//     {
+//         name: "Odyssey",
+//         page: "438",
+//         genre: "epic poem",
+//         author: "Homer"
+//     },
+//     {
+//         name: "Faust",
+//         page: "522",
+//         genre: "tragic",
+//         author: "Goethe"
+//     },
+//     {
+//         name: "Histories",
+//         page: "763",
+//         genre: "History",
+//         author: "Herodotus"
+//     }
+// ];
+// console.log(book [0]);
+// console.log(book [1]);
+// console.log(book [2]);
+
+
+
+//5
+// let height = 23;
+// let width = 10;
+// let s = (height * width);
+// console.log(s + "cm^2");
+
+
+
+//6
+// let heightC = 10;
+// let dc = 4;
+// let v = (Math.PI * Math.pow(dc/2, 2) * heightC);
+// console.log(v);
+
+
+
+//7
+// let n = 3;
+// let m = 4;
+// let k = (Math.pow(n, 2) + Math.pow(m, 2));
+// console.log(k);
