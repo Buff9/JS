ebc386ea6d0714bf09f4b8ab69582429004b43b8
Buff9 <yura.korovickiy9583@gmail.com>
2021-11-14 19:46:13 +0300

    1

---
+++ lesson2/Additional/script.js
@@ -0,0 +1,30 @@
+//1.
+// let friends = ["Ivan", "Slavik", "Grisha", "Lera", "Ira"];
+// let a = friends.length;
+// if (a >= 3) {
+//     alert('це великий масив');
+// }else{
+//     alert('це маленький масив');
+// }
+
+
+
+//2.
+// let a = +prompt('first value');
+// let b = +prompt('second value');
+// let c = +prompt('third value');
+// let middle = [a, b, c].sort( (a, b) => a - b)[1];
+//
+// console.log(middle);
+
+
+
+//3.
+// let result  = (a + b < 4) ? 'Мало' : 'Багато';
+
+
+
+//4.
+// let num = +prompt('enter your num') ;
+// let number = num < 0 ? 'negative' : num === 0 ? 'null' : num > 0 ? 'positive' : '';
+// console.log(number);
