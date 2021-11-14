1b1bc44bb40fe6818b3398165538e22e4987e2b1
Buff9 <yura.korovickiy9583@gmail.com>
2021-11-14 18:38:23 +0300

    1

---
+++ lesson2/homework/script.js
@@ -0,0 +1,102 @@
+//1.
+// let time = +prompt('Число від 1 до 59');
+// if (time <= 15) {
+//     alert('First quarter');
+// } else if (time >= 16 && time <= 30) {
+//     alert('Second quarter');
+// } else if (time >= 31 && time <= 45) {
+//     alert('Third quarter');
+// } else if (time >= 46 && time <= 59) {
+//     alert('Fourth quarter');
+// } else if (time > 59) {
+//     alert('Invalid Value');
+// }
+
+
+
+//2.
+// let day = +prompt('Число від 1 до 31');
+// if (day <= 10) {
+//     alert('First Decade');
+// } else if (day >= 11 && day <= 20) {
+//     alert('Second Decade');
+// } else if (day >= 21 && day <= 31) {
+//     alert('Third Decade');
+// }else if (day > 31) {
+//     alert('Invalid Value');
+// }
+
+
+
+//3.1
+// let test = prompt();
+// let test1 = test === 'correct answer' ? 'вірно' : 'не вірно';
+// alert(test1);
+
+//3.2
+// let test = prompt();
+// if (test === 'correct answer'){
+//     alert('вірно');
+// }else{
+//     alert('не вірно');
+// }
+
+
+
+//4.
+// let a = +prompt('enter number');
+// if (a === 0) {
+//     alert('не вірнно');
+// }else{
+//     alert('вірно');
+// }
+
+
+
+//5.
+// let i = +prompt('enter day');
+// switch (i){
+//     case 1:
+//         alert('daily schedule1');
+//         break;
+//     case 2:
+//         alert('daily schedule2');
+//         break;
+//     case 3:
+//         alert('daily schedule3');
+//         break;
+//     case 4:
+//         alert('daily schedule4');
+//         break;
+//     case 5:
+//         alert('daily schedule5');
+//         break;
+//     case 6:
+//         alert('daily schedule6');
+//         break;
+//     case 7:
+//         alert('daily schedule7');
+//         break;
+//     default:
+//         alert('Invalid Value');
+// }
+
+
+
+//6.
+// let year = +prompt('number of days in the year?');
+// if (year / 2 === 183){
+//     alert('високосний');
+// }else{
+//     alert('не високосний');
+// }
+
+
+
+//7.
+// let a = prompt('Яка «офіційна» назва JavaScript?');
+// if (a === 'ECMAScript') {
+//     alert('Правильно!');
+// }else{
+//     alert('Не знаєте? ECMAScript!');
+// }
