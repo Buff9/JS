db0c226dcfa22e3ddd2629f395ff3b7a716a0fa2
Buff9 <yura.korovickiy9583@gmail.com>
2021-11-15 17:00:59 +0300

    1

---
+++ lesson2/Classwork/Script.js
@@ -0,0 +1,88 @@
+//1.
+// let a = +prompt('First Value');
+// let b = +prompt('Second Value');
+// if (a === b) {
+//     alert('Числа рівні');
+// }else{
+//     alert(Math.max(a, b));
+// }
+
+
+
+//2.
+// let house1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// let house2 = [21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
+// let house3 = [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
+// let a = +prompt('Номер квартири');
+// if (house1.includes(a)){
+//     alert('Підїзд №1');
+// }else if (house2.includes(a)){
+//     alert('Підїзд №2');
+// }else if (house3.includes(a)){
+//     alert('Підїзд №3');
+// }else{
+//     alert('Invalid Value');
+// }
+
+
+
+//3.
+// let number = +prompt('your number');
+// if (number === 10){
+//     alert('ВІРНО');
+// }else{
+//     alert('НЕВІРНО');
+// }
+
+
+
+//4.
+// let x = [56, 45, 36];
+// if (typeof x == 'number'){
+//     console.log('1');
+// }else if( typeof x == 'string'){
+//     console.log('2');
+// }else if (typeof x == 'boolean'){
+//     console.log('3');
+// }else if (typeof x == 'object'){
+//     console.log('4');
+// }
+
+
+
+//5.
+// let q = prompt('Message?');
+// if (q === 'чи ми йдемо сьогодні в OKTEN на навчання?'){
+//     t = +prompt('яка температурa?');
+//     if (t >= 10 && t <= 22){
+//         alert('ми йдемо ВЧИТИСЯ');
+//     }else{
+//         alert('сидимо вдома і вчимося ОНЛАЙН');
+//     }
+// }else {
+//     alert('unknown message');
+// }
+
+
+
+//6.
+// x = +prompt('Enter your number');
+// switch (x){
+//     case 1:
+//         alert('Авто');
+//         break;
+//     case 2:
+//         alert('Мото');
+//         break;
+//     case 3:
+//         alert('Телефон');
+//         break;
+//     case 4:
+//         alert('Notebook');
+//         break;
+//     case 5:
+//         alert('PC');
+//         break;
+//     default:
+//         alert('число не вірне');
+// }
