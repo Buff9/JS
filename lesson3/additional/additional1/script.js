eed2ea425a13950fdfafb1099edcf7312796c36d
Buff9 <yura.korovickiy9583@gmail.com>
2021-11-17 21:32:00 +0300

    1

--- lesson3/additional/additional1/script.js
+++ lesson3/additional/additional1/script.js
@@ -597,7 +597,7 @@ let post = [
         userId: 10,
         id: 100,
         title: 'at nam consequatur ea labore ea harum',
-        body: 'cupiditate quo est a modi nesciunt soluta\nipsa voluptas error itaque dicta in\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut'
+        body: 'cupiditate quo est a mod nesciunt soluta\nipsa voluptas error itaque dicta in\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut'
     }
 ];
 for (let postElement of post) {
